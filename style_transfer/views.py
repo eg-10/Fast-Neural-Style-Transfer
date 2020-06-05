0d72d3b3d28c8bc22f7bdb78cf059f9541c69a55
Ebrahim Gabajiwala <ebrahimgabajiwala@gmail.com>
2020-06-06 01:41:21 +0530

    Created style selection interface

--- style_transfer/views.py
+++ style_transfer/views.py
@@ -3,17 +3,19 @@ import time
 
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 from style_transfer.forms import UploadContentForm
-from django.conf import settings
+from style_transfer.models import Style
 
 
 def home(request):
     content = None
+    styles = Style.objects
     if request.method == 'POST' and request.FILES['content']:
         content = request.FILES['content']
         fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 
                                                     'content'))
         fs.save(content.name,content)
         print(content.name)
-    return render(request, 'style_transfer/index.html', {'content':content})
+    return render(request, 'style_transfer/index.html', {'content':content , 'styles':styles})
